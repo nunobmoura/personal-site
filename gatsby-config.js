d3b06bf1bd5ef6c87a3e268cfd50bd22ab1d8963
nunobmoura <nunobaratamoura@gmail.com>
2021-09-22 11:47:27 +0100

    Performance improvement

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,8 @@ module.exports = {
         display: `standalone`,
         icon: `src/assets/icon.svg`
       }
-    }
+    },
+    `gatsby-plugin-minify`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
