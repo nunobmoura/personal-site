c781da614dbf9ab8bad731eff4da37259ec8b2c7
nunobmoura <nunobaratamoura@gmail.com>
2022-02-04 20:47:58 +0000

    Fix carrer container responsivness

--- src/sections/carreer.js
+++ src/sections/carreer.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react"
 
-import Experience from '../components/experience'
-import Education from '../components/education'
+import Experience from "../components/experience"
+import Education from "../components/education"
 
-import * as carreerStyles from './carreer.module.scss'
+import * as carreerStyles from "./carreer.module.scss"
 
 const Carreer = () => (
-  <section className={`${carreerStyles.carreerContainer} grid-container carreer-container`}>
+  <section
+    className={`${carreerStyles.carreerContainer} grid-container carreer-container`}
+  >
     <Experience />
     <Education />
   </section>
