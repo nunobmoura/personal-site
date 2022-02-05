ae1bd42a421c568c691a69181d5688fce0f41b56
nunobmoura <nunobaratamoura@gmail.com>
2022-02-05 11:57:08 +0000

    Index and Post creaton

---
+++ src/pages/projects/posts.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import showdown from "showdown"
+
+const Post = () => {
+  const converter = new showdown.Converter()
+
+  const data = useStaticQuery(graphql`
+    query {
+      contentfulProject {
+        title
+        company
+        companysLink
+        date
+        content {
+          content
+        }
+        link
+        withWho
+      }
+    }
+  `)
+
+  const body = data.contentfulProject.content.content
+  
+  return (
+    <div>
+      <h1>Post</h1>
+      <p>{data.contentfulProject.title}</p>
+      <p>{data.contentfulProject.company}</p>
+      <p>{data.contentfulProject.companysLink}</p>
+      <p>{data.contentfulProject.date}</p>
+
+      <div dangerouslySetInnerHTML={{ __html: converter.makeHtml(body) }} />
+
+      <p>{data.contentfulProject.link}</p>
+      <p>{data.contentfulProject.withWho}</p>
+    </div>
+  )
+}
+
+export default Post
