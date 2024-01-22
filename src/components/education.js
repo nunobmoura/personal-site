b187b861ddffff859587666546e767d8ef2142b4
nunobmoura <nunobaratamoura@gmail.com>
2024-01-22 21:26:05 +0000

    update finishDate on education

--- src/components/education.js
+++ src/components/education.js
@@ -6,13 +6,8 @@ import * as educationStyles from './edu-exp.module.scss'
 const Education = () => {
   const data = useStaticQuery(graphql`
     query {
-      allContentfulEducation (
-        sort: {
-          fields: startDate,
-          order: DESC
-        }
-      ) {
-        edges {
+      allContentfulEducation (sort: {finishDate: DESC}) {
+        edges{
           node {
             startDate (formatString:"MMM YYYY")
             finishDate (formatString:"MMM YYYY")
@@ -31,7 +26,13 @@ const Education = () => {
       <ol>
         {data.allContentfulEducation.edges.map((edge) => (
           <li key={edge.node.courseName.replace(/\s/g, '')}>
-            <h3>{edge.node.startDate ? `${edge.node.startDate} - ${edge.node.finishDate}` : `Ongoing`}</h3>
+            <h3>
+            {edge.node.startDate && edge.node.finishDate
+            ? `${edge.node.startDate} - ${edge.node.finishDate}`
+            : edge.node.finishDate
+            ? edge.node.finishDate
+            : 'Ongoing'}
+            </h3>
             <p>
               {edge.node.link ? <a href={edge.node.link} title={`Link to ${edge.node.courseName}`} target="_blank" rel="noreferrer">{edge.node.courseName}</a> : edge.node.courseName}
               {edge.node.courseDetails && ` - ${edge.node.courseDetails}`}
